db4ce24aca38f7e2e88ec8831d2f2e50f10f3700
Juan Gabardini <jgabardini@computer.org>
2017-05-19 17:23:37 +0200

    manejo palabra completa

--- app.rb
+++ app.rb
@@ -1,5 +1,27 @@
 require 'sinatra'
 
+PALABRA = "repollo"
+
 get '/' do
+    erb :index
+end
+
+post '/letra' do
+	letra=  params[:letra]
+	if PALABRA.include? letra.downcase
+		@mensaje= "Acertaste! #{letra} existe en la palabra secreta"
+	else
+		@mensaje= "#{letra} no existe en la palabra secreta"
+	end		
+    erb :index
+end
+
+post '/palabra' do
+	palabra=  params[:palabra]
+	if PALABRA == palabra.downcase
+		@mensaje= "Ganaste! #{palabra} es la palabra secreta"
+	else
+		@mensaje= "#{palabra} no es la palabra secreta"
+	end		
     erb :index
 end
--- spec/ahorcado_spec.rb
+++ spec/ahorcado_spec.rb
@@ -1,7 +1,37 @@
-describe  '' do
-	it 'hi' do
+#
+
+describe  'Ahorcado' do
+	it 'Nombre de la aplicación' do
 		visit '/'
-		expect(page).to have_content 'Ahorcado'
+		expect(page.find('h1').text).to include('Ahorcado')
+	end
+
+	it 'X no está en repollo' do
+		visit '/'
+		fill_in :letra, :with => 'X'
+		click_button :una_letra
+		expect(page.find('#mensaje').text).to include('no existe')
+	end
+
+	it 'E está en repollo' do
+		visit '/'
+		fill_in :letra, :with => 'E'
+		click_button :una_letra
+		expect(page.find('#mensaje').text).to include('Acertaste')
+	end
+
+	it 'casa no es repollo' do
+		visit '/'
+		fill_in :palabra, :with => 'casa'
+		click_button :una_palabra
+		expect(page.find('#mensaje').text).to include('no es')
+	end
+
+	it 'repollo es repollo. Ganaste' do
+		visit '/'
+		fill_in :palabra, :with => 'repollo'
+		click_button :una_palabra
+		expect(page.find('#mensaje').text).to include('Ganaste')
 	end
 
 end 
